4f34cc32dba89014625641713bde848b5fcccd8c
Mengliang.Huang <hml1006@qq.com>
2023-06-27 14:13:43 +0800

    Add create compile_commands.json script
    Signed-off-by: Mengliang.Huang <hml1006@qq.com>

---
+++ scripts/clang-tools/mk_vscode_symbols.sh
@@ -0,0 +1,4 @@
+sudo apt install gcc-aarch64-linux-gnu
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- menuconfig
+make CROSS_COMPILE=aarch64-linux-gnu- ARCH=arm64
+./scripts/clang-tools/gen_compile_commands.py
