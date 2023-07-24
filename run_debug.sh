05403cb0071122f92cc91b04b2a3788c9bd39bf0
Mengliang.Huang <hml1006@qq.com>
2023-07-24 17:16:20 +0800

    更新配置和脚本 1.clangd解析配置 2.qemu运行脚本 3.vscode调试配置

--- run_debug.sh
+++ run_debug.sh
@@ -3,7 +3,9 @@
 qemu-system-aarch64 -cpu cortex-a57 -machine virt \
   -m 4096 -smp 2 \
   -kernel arch/arm64/boot/Image \
-  -nographic --fsdev local,id=kmod_dev,path=$PWD/k_shared,security_model=none \
-  -device virtio-9p-device,fsdev=kmod_dev,mount_tag=kmod_mount -S -s \
+  --fsdev local,id=kmod_dev,path=$PWD/k_shared,security_model=none \
+  -device virtio-9p-device,fsdev=kmod_dev,mount_tag=kmod_mount \
   --append "rdinit=/linuxrc root=/dev/ram rw loglevel=8 console=ttyAMA0"  \
-  -initrd ./rootfs.cpio.gz
+  -serial stdio \
+  -initrd ./rootfs.cpio.gz -S -s
+  #-S -s
