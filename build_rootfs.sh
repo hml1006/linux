1389cf8eb464d08cb83e5c61bd5c9308d9fc9c5d
hml1006 <hml1006@qq.com>
2024-03-18 23:33:52 +0800

    rootfs安装systemd

--- build_rootfs.sh
+++ build_rootfs.sh
@@ -4,8 +4,6 @@
 # https://qubot.org/2023/08/09/h618-%E7%A7%BB%E6%A4%8Dubuntu-22-04-rootfs/
 
 sudo apt-get install qemu-user-static
-sudo rm -rf rootfs
-mkdir rootfs
 if [ ! -e ubuntu-base-23.10-base-arm64.tar.gz ];then
     wget http://cdimage.ubuntu.com/ubuntu-base/releases/mantic/release/ubuntu-base-23.10-base-arm64.tar.gz
 fi
@@ -18,8 +16,11 @@ fi
 if [ ! -e openssl_3.0.10-1ubuntu4_arm64.deb ];then
     wget http://ports.ubuntu.com/pool/main/o/openssl/openssl_3.0.10-1ubuntu4_arm64.deb
 fi
+if [ ! -e rootfs ];then
+    mkdir rootfs
+    tar -xzvf ubuntu-base-23.10-base-arm64.tar.gz -C rootfs
+fi
 
-tar -xzvf ubuntu-base-23.10-base-arm64.tar.gz -C rootfs
 sudo cp *.deb ./rootfs/root
 sudo cp /etc/resolv.conf ./rootfs/etc/
 # sudo cp /etc/apt/sources.list ./rootfs/etc/apt/
@@ -35,6 +36,8 @@ echo "dpkg -i openssl_3.0.10-1ubuntu4_arm64.deb" | sudo tee -a ./rootfs/root/ins
 echo "dpkg -i ca-certificates_20230311ubuntu1_all.deb" | sudo tee -a ./rootfs/root/install.sh
 echo "chmod 777 /tmp/" | sudo tee -a ./rootfs/root/install.sh
 echo "apt update" | sudo tee -a ./rootfs/root/install.sh
+echo "apt install -y systemd sudo vim nano kmod net-tools ethtool ifupdown rsyslog htop iputils-ping language-pack-en-base ssh" | sudo tee -a ./rootfs/root/install.sh
+echo "ln -s /lib/systemd/system/getty\@.service /etc/systemd/system/getty.target.wants/getty\@ttyAMA0.service" | sudo tee -a ./rootfs/root/install.sh
 
 sudo chmod +x ./rootfs/root/install.sh
 sudo cp /usr/bin/qemu-arm-static ./rootfs/usr/bin/
@@ -50,11 +53,10 @@ sudo umount ./rootfs/dev/
 
 dd if=/dev/zero of=ubuntu-rootfs.img bs=1M count=2048
 sudo mkfs.ext4  ubuntu-rootfs.img
+rm -rf ubuntu-mount
 mkdir ubuntu-mount
 sudo mount ubuntu-rootfs.img ubuntu-mount/
 sudo cp -rfp rootfs/*  ubuntu-mount/
 sudo umount ubuntu-mount/
 sudo rm -rf ubuntu-mount/
 e2fsck -p -f ubuntu-rootfs.img
-
-sudo rm -rf rootfs
