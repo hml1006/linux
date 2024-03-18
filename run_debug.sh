69c8bf7b7c2279b1095dd3914a3051f608b51de0
Mengliang.Huang <hml1006@qq.com>
2024-03-18 20:01:14 +0800

    重新设置gdb配置

--- run_debug.sh
+++ run_debug.sh
@@ -15,16 +15,27 @@ elif [ ! -d "k_shared" ];then
   mkdir k_shared
 fi
 
+cur_dir=`pwd`
+gdb_cfg=${HOME}/.config/gdb
+mkdir -p ${gdb_cfg}
+echo "add-auto-load-safe-path ${cur_dir}/.gdbinit"
+echo "set auto-load python-scripts on" > .gdbinit
+echo "add-auto-load-safe-path ${cur_dir}/" >> .gdbinit
+echo "source ${cur_dir}/add-auto-load-safe-path /home/louis/code/linux/" >> .gdbinit
+echo "set architecture aarch64" >> .gdbinit
+echo "file vmlinux" >> .gdbinit
+echo "b start_kernel" >> .gdbinit
+
 sudo cp qemu-ifup /etc/
 
 sudo qemu-system-aarch64 -cpu cortex-a57 -machine virt \
   -m 4096 -smp 2 \
   -net tap,ifname=tap0 -net nic \
-  -drive file=nvme.img,if=none,id=nvm0 \
+  -drive file=nvme.img,format=raw,if=none,id=nvm0 \
   -device nvme,serial=12345678,drive=nvm0 \
   -kernel arch/arm64/boot/Image \
   --fsdev local,id=kmod_dev,path=$PWD/k_shared,security_model=none \
   -device virtio-9p-device,fsdev=kmod_dev,mount_tag=kmod_mount \
   --append "root=/dev/vda rootfstype=ext4 rw loglevel=8 console=ttyAMA0"  \
   -serial stdio \
-  -hda ./ubuntu-rootfs.img -S -s
+  -drive file=ubuntu-rootfs.img,index=0,media=disk,format=raw -S -s
